0f7a8f047d863a29a0ffa8b9df16cd84f10dffd1
Sean Kelly <sean.kelly@ruralsource.com>
2019-02-25 16:42:19 -0500

    Adding some authentication prep

--- src/server/routes/api/auth.js
+++ src/server/routes/api/auth.js
@@ -1,5 +1,25 @@
 import mongoose from 'mongoose';
+import express from 'express';
 import passport from 'passport';
 
 const User = mongoose.Schema('User');
+const router = express.Router();
 
+router.use('/login', (req, res) => {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).json({
+        message: 'something is not right'
+      });
+    }
+
+    return req.login(user, { session: false }, (loginErr) => {
+      if (loginErr) {
+        return res.send(loginErr);
+      }
+
+      const token = null;
+      return res.json({ user: token });
+    });
+  })(req, res);
+});
